3a61106ed57fa458c242c25034ca146f10c6fb21
github username <github email address>
2020-02-13 14:08:13 -0700

    updates places controller for dynamic search on frontend by price, category, and location

--- backendMod2ProjectJapanForGithub/app/controllers/places_controller.rb
+++ backendMod2ProjectJapanForGithub/app/controllers/places_controller.rb
@@ -4,8 +4,12 @@ class PlacesController < ApplicationController
         if params[:location] && params[:location] != "null"
             @places = Place.where(location: params[:location])
             render json: @places
-        # elsif
-
+        elsif params[:price] && params[:price] != "null"
+            @places = Place.where(price: params[:price])
+            render json: @places
+        elsif params[:category] && params[:category] != "null"
+            @places = Place.where(category: params[:category])
+            render json: @places
         else
             @places = Place.all
             render json: @places
